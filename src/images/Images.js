62aa61abfbfe0ce9c97d139e1a9cd30241f8f6ed
saiaravind0014 <85366375+saiaravind0014@users.noreply.github.com>
2024-02-28 16:37:01 +0530

    upload doc

---
+++ src/images/Images.js
@@ -0,0 +1,10 @@
+import nsic from '../images/nsic.jpg';
+import jorim from '../images/jorim.jpg';
+import innovik from '../images/innovik.jpeg';
+import chaitanya from '../images/chaitanya.jpg';
+import tirumala from '../images/tirumala.jpg';
+import sairam from '../images/sairam.jpg';
+
+export const Img = {
+    nsic,jorim,innovik,chaitanya,tirumala,sairam
+}
