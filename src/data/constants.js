6c96666e85a921cc73cc11fab9bbcac833160a6f
Sai Aravind Appalabhakthula <saiaravind0014@gmail.com>
2024-07-15 11:17:48 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,8 +9,8 @@ export const Bio = {
     description:
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/saiaravind0014",
-    // resume:
-    //   "https://drive.google.com/file/d/1aWq1SJzQHJmRM13Gfizl4V0ugz1pQ6Dm/view?usp=sharing",
+    resume:
+      "https://drive.google.com/file/d/1aWq1SJzQHJmRM13Gfizl4V0ugz1pQ6Dm/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/sai-aravind-appalabhakthula-242122214/",
   };
   
